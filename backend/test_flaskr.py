4a06389e09c0897551db9d977227b3bdfa08273d
Shaurav Thapa <shauravofficial@gmail.com>
2020-09-07 18:50:26 +0100

    feat add test delete category and question

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -43,7 +43,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['categories']))
 
-    def test_post_categories(self):
+    def test_post_category(self):
         post_data = {
             'type': 'a'
         }
@@ -53,6 +53,24 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
+    # def test_delete_category(self):
+    #     res = self.client().delete('/categories/15')
+    #     data = json.loads(res.data)
+
+    #     category = Category.query.filter(Category.id == 17).one_or_none()
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertEqual(data['deleted'], 17)
+    #     self.assertEqual(category, None)
+
+    def test_404_if_category_does_not_exist(self):
+        res = self.client().delete('/categories/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Category with id:1000 not found')
 
     def test_get_paginated_questions(self):
         res = self.client().get('questions')
@@ -84,6 +102,26 @@ class TriviaTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
+
+    # def test_delete_question(self):
+    #     res = self.client().delete('/questions/10')
+    #     data = json.loads(res.data)
+
+    #     question = Question.query.filter(Question.id == 10).one_or_none()
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertEqual(data['deleted'], 10)
+    #     self.assertEqual(question, None)
+
+    def test_404_if_question_does_not_exist(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Question with id:1000 not found')
+
 
 
 # Make the tests conveniently executable
